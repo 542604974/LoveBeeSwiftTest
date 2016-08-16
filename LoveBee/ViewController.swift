8c377b82f7366704c65da4c54c0a572e187079d6
陈伟光 <542604974@qq.com>
2016-08-16 21:26:55 +0800

    Initial Commit

---
+++ LoveBee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveBee
+//
+//  Created by apple on 16/8/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
